1c89e8785fd65ebb1f4c46a91a409ee1916fe944
Thom Sax <sacchinit@gmail.com>
2024-01-05 17:37:07 +0100

    add generic

--- Home.py
+++ Home.py
@@ -21,67 +21,67 @@ if span=='Custom':
 geolocator = Nominatim(user_agent="abcd")
 
 city = f'{citta}, {paese}'
-try:
-    if len(citta)>0:
-        address=geolocator.geocode(city)
-        lat=address.latitude
-        lon=address.longitude
-        url = f"https://api.opentopodata.org/v1/aster30m?locations={lat},{lon}"
-        r = requests.get(url)
+#try:
+if len(citta)>0:
+    address=geolocator.geocode(city)
+    lat=address.latitude
+    lon=address.longitude
+    url = f"https://api.opentopodata.org/v1/aster30m?locations={lat},{lon}"
+    r = requests.get(url)
 
-        data = r.json()
-        elev=data['results'][0]['elevation']
+    data = r.json()
+    elev=data['results'][0]['elevation']
 
-        # Set time period
-        end = oggi
-        if span=='1W':
-            start = oggi-relativedelta(weeks=1)
-        elif span=='1M':
-            start = oggi-relativedelta(months=1)
-        elif span=='6M':
-            start = oggi-relativedelta(months=6)
-        elif span=='1Y':
-            start = datetime(oggi.year-1,oggi.month,oggi.day)
-        elif span=='5Y':
-            start = datetime(oggi.year-5,oggi.month,oggi.day)
-        elif span=='10Y':
-            start = datetime(oggi.year-10,oggi.month,oggi.day)
-        elif span=='Max':
-            start = datetime(1900,1,1)
+    # Set time period
+    end = oggi
+    if span=='1W':
+        start = oggi-relativedelta(weeks=1)
+    elif span=='1M':
+        start = oggi-relativedelta(months=1)
+    elif span=='6M':
+        start = oggi-relativedelta(months=6)
+    elif span=='1Y':
+        start = datetime(oggi.year-1,oggi.month,oggi.day)
+    elif span=='5Y':
+        start = datetime(oggi.year-5,oggi.month,oggi.day)
+    elif span=='10Y':
+        start = datetime(oggi.year-10,oggi.month,oggi.day)
+    elif span=='Max':
+        start = datetime(1900,1,1)
+    else:
+        start = ran_date[0]
+        end = ran_date[1]
+
+    # Create Point
+    citta = Point(lat, lon,elev)
+
+    # Get daily data
+    data = Daily(citta, start, end)
+    df_temp = data.fetch()
+    df_temp = df_temp.rename(columns={'tavg':'Average','tmax':'Temp Max','tmin':'Temp Min'})
+
+    st.text('Temperatures over the selected period:')
+    col1, col2=st.columns([0.15,0.85])
+    with col1:
+        tmp_type=st.multiselect('Choose what you wanna see',['Average','Temp Max','Temp Min'],['Average'])
+        agg_type=st.radio('Type of aggregation:',['By Day','By Month','By Year'])
+    with col2:
+        df_temp['Day']=df_temp.index
+        if agg_type=='By Month':
+            df_temp['ym']=[str(x.year)+'{:02d}'.format(x.month) for x in df_temp['Day']]
+            df_to_plot = df_temp.groupby('ym',as_index=False).agg({'Average':'mean','Temp Max':'mean','Temp Min':'mean'})
+            df_to_plot = df_to_plot[tmp_type]
+            st.line_chart(df_to_plot,x='ym',y=tmp_type)
+        elif agg_type=='By Year':
+            df_temp['year']=[x.year for x in df_temp['Day']]
+            df_to_plot = df_temp.groupby('year',as_index=False).agg({'Average':'mean','Temp Max':'mean','Temp Min':'mean'})
+            df_to_plot = df_to_plot[tmp_type]
+            st.line_chart(df_to_plot,x='year',y=tmp_type)
         else:
-            start = ran_date[0]
-            end = ran_date[1]
+            df_to_plot = df_temp[tmp_type]
+            st.line_chart(df_to_plot)
 
-        # Create Point
-        citta = Point(lat, lon,elev)
-
-        # Get daily data
-        data = Daily(citta, start, end)
-        df_temp = data.fetch()
-        df_temp = df_temp.rename(columns={'tavg':'Average','tmax':'Temp Max','tmin':'Temp Min'})
-
-        st.text('Temperatures over the selected period:')
-        col1, col2=st.columns([0.15,0.85])
-        with col1:
-            tmp_type=st.multiselect('Choose what you wanna see',['Average','Temp Max','Temp Min'],['Average'])
-            agg_type=st.radio('Type of aggregation:',['By Day','By Month','By Year'])
-        with col2:
-            df_temp['Day']=df_temp.index
-            if agg_type=='By Month':
-                df_temp['ym']=[str(x.year)+'{:02d}'.format(x.month) for x in df_temp['Day']]
-                df_to_plot = df_temp.groupby('ym',as_index=False).agg({'Average':'mean','Temp Max':'mean','Temp Min':'mean'})
-                df_to_plot = df_to_plot[tmp_type]
-                st.line_chart(df_to_plot,x='ym',y=tmp_type)
-            elif agg_type=='By Year':
-                df_temp['year']=[x.year for x in df_temp['Day']]
-                df_to_plot = df_temp.groupby('year',as_index=False).agg({'Average':'mean','Temp Max':'mean','Temp Min':'mean'})
-                df_to_plot = df_to_plot[tmp_type]
-                st.line_chart(df_to_plot,x='year',y=tmp_type)
-            else:
-                df_to_plot = df_temp[tmp_type]
-                st.line_chart(df_to_plot)
-
-        st.text('Where we are:')
-        st.map(pd.DataFrame({'Lat':[lat],'Lon':[lon]}),latitude='Lat',longitude='Lon',zoom=6,color='#FF0000')
-except:
-    st.error("Error: city not found!")
+    st.text('Where we are:')
+    st.map(pd.DataFrame({'Lat':[lat],'Lon':[lon]}),latitude='Lat',longitude='Lon',zoom=6,color='#FF0000')
+#except:
+#    st.error("Error: city not found!")
